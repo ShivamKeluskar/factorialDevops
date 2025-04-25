07b01cfc2bbb3afecf29ac2e2108c05a2c0f1b95
HarshalBhosle <harsalb1202@gmail.com>
2025-04-25 09:09:34 +0530

    first commit

---
+++ src/factoiral.java
@@ -0,0 +1,28 @@
+
+public class factoiral {
+
+	    // Iterative approach
+	    public static long iterativeFactorial(int n) {
+	        long result = 1;
+	        for (int i = 1; i <= n; i++) {
+	            result *= i;
+	        }
+	        return result;
+	    }
+
+	    // Recursive approach
+	    public static long recursiveFactorial(int n) {
+	        if (n == 0) {
+	            return 1;
+	        } else {
+	            return n * recursiveFactorial(n - 1);
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        int number = 5;
+	        System.out.println("Factorial of " + number + " (iterative): " + iterativeFactorial(number));
+	        System.out.println("Factorial of " + number + " (recursive): " + recursiveFactorial(number));
+	    }
+
+}
